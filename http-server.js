d5ee94ec770dd2c19ae36e191fec629f4b8c4969
Lionel Landwerlin <llandwerlin@gmail.com>
2014-01-24 15:31:30 +0000

    remove unused code

--- http-server.js
+++ http-server.js
@@ -6,40 +6,6 @@ const GLib = imports.gi.GLib;
 const Gio = imports.gi.Gio;
 const Soup = imports.gi.Soup;
 
-const ByteArray = imports.byteArray;
-
-/* HTTP handler */
-
-let handlers = {};
-
-let registerHandler = function(path, handler) {
-    log('Register handler for ' + path);
-    handlers[path] = handler;
-};
-
-let findHandler = function(path) {
-    let ret = path.replace("//", "/");
-    ret = ret.replace("//", "/");
-    return handlers[ret];
-};
-
-/* Files */
-
-let filesHash = {}
-let addFile = function(httpPath, localPath) {
-    let f = {
-        httpPath: httpPath,
-        localPath: localPath,
-    };
-    filesHash[httpPath] = f;
-};
-
-let findFileFromPath = function(path) {
-    let p = path.replace("//", "/");
-    p = p.replace("//", "/");
-    return filesHash[p];
-};
-
 /**/
 
 let directories = [];
@@ -56,43 +22,6 @@ let findFileFromPath = function(path) {
     }
     return null;
 };
-
-/* updates.xml generation */
-
-let checksumString = function(str) {
-    return GLib.compute_checksum_for_string(GLib.ChecksumType.SHA256, str, str.length);
-};
-
-let checksumBytes = function(bytes) {
-    return GLib.compute_checksum_for_bytes(GLib.ChecksumType.SHA256, bytes);
-};
-
-let compress = function(str) {
-    let tmpfile = new Gio.File.new_for_path('/tmp/plop');
-    try {
-        tmpfile.delete(null);
-    } catch (ex) {
-        log(ex.message);
-    }
-
-    let output = new Gio.ConverterOutputStream({
-    base_stream: tmpfile.create(Gio.FileCreateFlags.REPLACE_DESTINATION, null),
-        converter: new Gio.ZlibCompressor({
-        format: Gio.ZlibCompressorFormat.GZIP,
-        }),
-    });
-
-    let bytes = GLib.Bytes.new(str, str.length);
-    //log('writing ' + bytes.get_size() + 'bytes');
-    output.write_bytes(bytes, null);
-    output.flush(null);
-    output.close(null);
-
-    let input = tmpfile.read(null);
-    let infos = tmpfile.query_info('standard::size', 0, null);
-    return input.read_bytes(infos.get_size(), null);
-};
-
 
 /* Handling updates payload */
 
